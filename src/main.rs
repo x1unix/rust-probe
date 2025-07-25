fb4b52ce96ac62539bb5f855bf0566cb69dddf56
x1unix <9203548+x1unix@users.noreply.github.com>
2025-07-24 23:51:05 -0400

    sync

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,13 @@ use std::env;
 pub mod kata;
 pub mod ugrep;
 
+struct Args(String, String);
+
+impl Args {
+    fn parse() -> Option<Self> {
+        let args: Vec<String> = env::args().skip(1).take(2).collect();
+    }
+}
 fn main() {
     let args: Vec<String> = env::args().collect();
     dbg!(&args);
