4c4ef50263c2e7cc5cb3ce1b20ace3665ab6b94f
x1unix <9203548+x1unix@users.noreply.github.com>
2025-07-25 11:59:46 -0400

    sync

--- src/ugrep/args.rs
+++ src/ugrep/args.rs
@@ -3,17 +3,27 @@ use std::env;
 pub struct Args {
     pub file_name: String,
     pub expr: String,
+    pub case_insensitive: bool,
 }
 
 impl Args {
     pub fn parse() -> Result<Self, &'static str> {
         // TODO: refactor into TryFrom
+
+        let case_insensitive = env::var("IGNORE_CASE")
+            .map(|s| s.parse::<bool>().unwrap_or(false))
+            .unwrap_or(false);
+
         let args: Vec<String> = env::args().skip(1).take(2).collect();
         if args.len() != 2 {
             return Err("not enough arguments. Usage: ugrep [expr] file_name");
         }
 
         let [expr, file_name]: [String; 2] = args.try_into().map_err(|_| "can't read arguments")?;
-        Ok(Args { file_name, expr })
+        Ok(Args {
+            file_name,
+            expr,
+            case_insensitive,
+        })
     }
 }
--- src/ugrep/run.rs
+++ src/ugrep/run.rs
@@ -2,6 +2,18 @@ use std::{error::Error, fs};
 
 use super::args::Args;
 
+pub fn search_case_insensitive<'a>(needle: &str, haystack: &'a str) -> Vec<&'a str> {
+    let query = needle.to_lowercase();
+    let mut results = Vec::with_capacity(32);
+    for line in haystack.lines() {
+        if line.to_lowercase().contains(&query) {
+            results.push(line);
+        }
+    }
+
+    results
+}
+
 pub fn search<'a>(needle: &str, haystack: &'a str) -> Vec<&'a str> {
     let mut results = Vec::with_capacity(32);
     for line in haystack.lines() {
@@ -14,10 +26,19 @@ pub fn search<'a>(needle: &str, haystack: &'a str) -> Vec<&'a str> {
 }
 
 pub fn run(args: &Args) -> Result<(), Box<dyn Error>> {
-    let Args { file_name, expr } = args;
+    let Args {
+        file_name,
+        expr,
+        case_insensitive,
+    } = args;
     let content = fs::read_to_string(file_name).expect("can't read file");
 
-    let results = search(expr, &content);
+    let results = if *case_insensitive {
+        search_case_insensitive(expr, &content)
+    } else {
+        search(expr, &content)
+    };
+
     for line in results {
         println!("{line}");
     }
